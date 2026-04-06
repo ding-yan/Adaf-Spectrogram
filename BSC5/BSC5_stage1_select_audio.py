6ef158236c6f8c22349027fb0334ea60d02a0b36
Hung-Hsuan Chen <hhchen1105@acm.org>
2026-04-07 07:56:34 +0800

    Make code more robust

--- BSC5/BSC5_stage1_select_audio.py
+++ BSC5/BSC5_stage1_select_audio.py
@@ -1,26 +1,31 @@
 import pandas as pd
 import shutil
 import os
+import sys
 
-# 路徑設定
-csv_path = 'C:\\Users\\User\\Desktop\\Dataset\\bird song data set\\bird_songs_metadata.csv'
-wav_src_folder = 'C:\\Users\\User\Desktop\\Dataset\\bird song data set\\wavfiles'
-wav_dst_folder = 'C:\\Users\\User\Desktop\\Dataset\\bird song data set\\selected_wavfiles'
+# path settings
+#csv_path = 'C:\\Users\\User\\Desktop\\Dataset\\bird song data set\\bird_songs_metadata.csv'
+csv_path = './data/bird_songs_metadata.csv'
+#wav_src_folder = 'C:\\Users\\User\Desktop\\Dataset\\bird song data set\\wavfiles'
+wav_src_folder = './data/wavfiles'
+#wav_dst_folder = 'C:\\Users\\User\Desktop\\Dataset\\bird song data set\\selected_wavfiles'
+wav_dst_folder = './data/selected_wavfiles'
 
-# 建立目標資料夾（如果不存在）
+# create target directory
 os.makedirs(wav_dst_folder, exist_ok=True)
 
-# 讀取 CSV 並取得 filename 欄位
+# read csv and obtain the 'filename' field
 df = pd.read_csv(csv_path)
 filenames = df['filename'].dropna().unique()
 
-# 複製檔案
+# copy files
 for fname in filenames:
     src_file = os.path.join(wav_src_folder, fname)
     dst_file = os.path.join(wav_dst_folder, fname)
     if os.path.exists(src_file):
         shutil.copy2(src_file, dst_file)
     else:
-        print(f"找不到檔案：{fname}")
+        print(f"Cannot find file：{fname}")
+        sys.exit(1)
 
-print("檔案複製完成。")
+print("File copy completed")
